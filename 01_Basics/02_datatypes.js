1a3949221796ab911dcf07823469b45177c4f326
iamsayannayek <sayannayek1912@gmail.com>
2023-12-14 18:07:01 +0530

    02_datatypes.js add

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,12 @@
+"use strict"; //treat all js code as newer verion of JS
+
+// Number 
+// bigInt 
+// null => standalone value
+// Symbol => unique value
+
+console.log(typeof null);
+// Output: Object
+
+console.log(typeof undefined);
+// Output: Undefined
