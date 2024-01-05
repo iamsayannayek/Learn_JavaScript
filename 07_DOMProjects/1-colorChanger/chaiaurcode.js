7a007e89eb0a8c077fac66d54ec525e9e7ff3b0c
iamsayannayek <sayannayek1912@gmail.com>
2024-01-05 09:17:39 +0530

    Update 07_DOMProjects

---
+++ 07_DOMProjects/1-colorChanger/chaiaurcode.js
@@ -0,0 +1,21 @@
+const button = document.querySelectorAll(".button");
+const body = document.querySelector('body');
+
+button.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+        switch (e.target.id) {
+            case 'grey':
+                body.style.backgroundColor = e.target.id;
+                break;
+            case 'white':
+                body.style.backgroundColor = e.target.id;
+                break;
+            case 'yellow':
+                body.style.backgroundColor = e.target.id;
+                break;
+            case 'blue':
+                body.style.backgroundColor = e.target.id;
+                break;
+        }
+    });
+});
